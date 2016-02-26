7706d07105d54fb407fc44c1bee54caca1ca2070
Marieke GUEYE <marieke@pryntcases.com>
2016-02-26 13:25:57 -0800

    add offline tag to collection

--- lib/craftar/collection.rb
+++ lib/craftar/collection.rb
@@ -1,6 +1,6 @@
 module Craftar
   class Collection < Craftar::Base
-    attr_reader :uuid, :name, :resource_uri
+    attr_reader :uuid, :name, :resource_uri, :offline
     def self.craftar_name
       'collection'
     end
@@ -9,17 +9,18 @@ module Craftar
       @name = opts[:name]
       @uuid = opts[:uuid]
       @resource_uri = opts[:resource_uri]
+      @offline = opts[:offline]
     end
 
     def save
-      response = json_call(:post, name: @name)
+      response = json_call(:post, name: @name, offline: @offline)
       @uuid = response['uuid']
       @resource_uri = response['resource_uri']
       self
     end
 
     def update(opts)
-      response = json_call(:put, { uuid: @uuid, name: opts[:name] }.select { |_, value| !value.nil? })
+      response = json_call(:put, { uuid: @uuid, name: opts[:name], offline: opts[:offline]}.select { |_, value| !value.nil? })
       @name = response['name']
       self
     end
